cdfb825cb7e19c3892971e2a5b38ebfc535f37f8
Garrett Hudson <garretthudson87@yahoo.com>
2020-09-29 11:51:57 -0400

    added components for the info bar and the input{

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+import './Chat.css';
+
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+
 let socket;
 
 const Chat = ({ location }) => {
@@ -51,8 +56,9 @@ const Chat = ({ location }) => {
   return (
     <div className="outerContainer">
       <div className="container">
-        <InfoBar />
+        <InfoBar room={room}/>
     {/* <input value={message} onChange={(event) => setMessage(event.target.value)} onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}/> */}
+        <Input message ={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
     </div>
   )
