3ff0d27a72f1881d9373c7491052bf92e0109b26
Garrett Hudson <garretthudson87@yahoo.com>
2020-09-29 12:49:54 -0400

    finished all the components, added emojis, changed touch icon, and it is running with no errors on local server

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,67 +1,68 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
-import io from 'socket.io-client';
+import io from "socket.io-client";
+
+import TextContainer from '../TextContainer/TextContainer';
+import Messages from '../Messages/Messages';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
 
 import './Chat.css';
 
-import InfoBar from '../InfoBar/InfoBar';
-import Input from '../Input/Input';
+const ENDPOINT = 'localhost:5000';
 
 let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'localhost:5000';
 
   useEffect(() => {
-    const {name, room } = queryString.parse(location.search);
+    const { name, room } = queryString.parse(location.search);
 
     socket = io(ENDPOINT);
 
-    setName(name);
     setRoom(room);
+    setName(name)
 
-    socket.emit('join', { name, room }, ({ error }) => {
-      
+    socket.emit('join', { name, room }, (error) => {
+      if(error) {
+        alert(error);
+      }
     });
-
-    return () => {
-      socket.emit('disconnect');
-
-      socket.off();
-    }
-    
   }, [ENDPOINT, location.search]);
-
+  
   useEffect(() => {
-    socket.on('message', (message) => {
-      setMessages([...messages, message]);
-    })
-  }, [messages]);
+    socket.on('message', message => {
+      setMessages(messages => [ ...messages, message ]);
+    });
+    
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+}, []);
 
-  // funmction for sending messages
   const sendMessage = (event) => {
     event.preventDefault();
 
-    if (message) {
+    if(message) {
       socket.emit('sendMessage', message, () => setMessage(''));
     }
   }
 
-  console.log(message, messages);
-
   return (
     <div className="outerContainer">
       <div className="container">
-        <InfoBar room={room}/>
-    {/* <input value={message} onChange={(event) => setMessage(event.target.value)} onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}/> */}
-        <Input message ={message} setMessage={setMessage} sendMessage={sendMessage} />
+          <InfoBar room={room} />
+          <Messages messages={messages} name={name} />
+          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
+      <TextContainer users={users}/>
     </div>
-  )
+  );
 }
 
 export default Chat;
