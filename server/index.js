6c52fd2740b1f07e81f81aa1aa87a0454bfb996d
Garrett Hudson <garretthudson87@yahoo.com>
2020-09-29 10:04:54 -0400

    finished the return for join.js component to render html in browser

--- server/index.js
+++ server/index.js
@@ -4,9 +4,21 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', () => {
+  console.log('we have a new connection');
+
+  socket.on('disconnect', () => {
+    console.log('User has left');
+  });
+});
+
+app.use(router);
 
 server.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
